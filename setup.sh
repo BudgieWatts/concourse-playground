b002c283d903c5aa3a0fbf89af52654744defac7
John Watts <drwatts.jw@googlemail.com>
2020-09-11 21:08:37 +0100

    A not very realistic pipeline example
    Build a thing.
    
    Run the thing.  In reality, you'd want to deploy the thing instead of running it.

--- setup.sh
+++ setup.sh
@@ -9,8 +9,8 @@ trap "echo stopping docker; docker-compose down; exit" SIGHUP SIGTERM SIGINT
 
 curl --retry 3 --retry-delay 5 "http://localhost:8080/api/v1/cli?arch=amd64&platform=$PLATFORM" > $FLY
 
-chmod +x ci/fly
+chmod +x $FLY
 
 $FLY -t ci login -c http://127.0.0.1:8080 -u test -p test
 
-$FLY -t ci set-pipeline -p booklit -c ./ci/booklit.yml
+$FLY -t ci set-pipeline -p pipeline -c ./ci/pipeline.yml
